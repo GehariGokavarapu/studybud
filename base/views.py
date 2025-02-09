590a1f25573baca7ea83f77c403d6f755eec765a
GehariGokavarapu <ggokavar@charlotte.edu>
2025-02-09 14:22:52 -0500

    Flash Messages

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -11,6 +12,16 @@ from .forms import RoomForm
 #     {'id': 2, 'name': 'Design with me'},
 #     {'id': 3, 'name': 'Frontend developers'},
 # ]
+
+def loginPage(request):
+            
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+
+
+
 def home(request): 
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
