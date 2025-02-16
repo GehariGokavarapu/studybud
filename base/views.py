f24b8dd20a975e0f7981a6838f063033d98732e6
GehariGokavarapu <ggokavar@charlotte.edu>
2025-02-16 03:05:15 -0500

    Restricted Pages

--- base/views.py
+++ base/views.py
@@ -37,7 +37,9 @@ def loginPage(request):
     return render(request, 'base/login_register.html', context)
 
 
-
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request): 
